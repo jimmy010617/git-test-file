0ae460c3f2714733f3d559ebfd02920a85a2ae11
jimmy010617 <jimmy990411@email.daelim.ac.kr>
2025-04-10 14:42:15 +0900

    1

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,5 +1,46 @@
 import './App.css';
 
-export default function Square() {
-  return <button className="square">X</button>;
+function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <div>
+      <button className='square' onClick={handleClick}>
+        {value}
+        </button>
+    </div>
+  );
+}
+
+function Board() {
+  return (
+    <div>
+      <div className='board-row'>
+        <Square value='1' />
+        <Square value='2' />
+        <Square value='3' />
+      </div>
+      <div className='board-row'>
+        <Square value='4' />
+        <Square value='5' />
+        <Square value='6' />
+      </div>
+      <div className='board-row'>
+        <Square value='7' />
+        <Square value='8' />
+        <Square value='9' />
+      </div>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Tic Tac Toe</h1>
+      <Board />
+    </div>
+  )
 }
